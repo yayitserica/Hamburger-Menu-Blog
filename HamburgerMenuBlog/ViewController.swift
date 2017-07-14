a3d09f63b6a713200339d254a92a7ff45953e600
Erica Millado <ericamillado@gmail.com>
2017-07-15 00:05:25 +0100

    Created Project

---
+++ HamburgerMenuBlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburgerMenuBlog
+//
+//  Created by Erica Millado on 7/15/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
